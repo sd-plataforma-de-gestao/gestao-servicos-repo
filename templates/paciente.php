48e1129572516a96ddb3d174f041dc4a4ec53208
Sauerrr <sauervitor12@gmail.com>
2025-09-16 19:36:13 -0300

    teste

--- templates/paciente.php
+++ templates/paciente.php
@@ -58,12 +58,12 @@ if (isset($_POST['salvar'])) {
   <link rel="icon" href="/assets/favicon.png" type="image/png">
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" />
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css" />
- <link rel="stylesheet" href="../styles/global.css">
-<link rel="stylesheet" href="../styles/header.css">
-<link rel="stylesheet" href="../styles/sidebar.css">
-<link rel="stylesheet" href="../styles/main.css">
-<link rel="stylesheet" href="../styles/responsive.css">
-<link rel="stylesheet" href="../styles/paciente.css">
+  <link rel="stylesheet" href="/styles/global.css">
+  <link rel="stylesheet" href="/styles/header.css">
+  <link rel="stylesheet" href="/styles/sidebar.css">
+  <link rel="stylesheet" href="/styles/main.css">
+  <link rel="stylesheet" href="/styles/responsive.css">
+  <link rel="stylesheet" href="/styles/paciente.css">
 </head>
 <body>
   <div id="header-container"></div>
